93ae4351a04cd86df9338e94b9b4583bd120d2c8
Ihsan Taha <ihsan_taha@yahoo.com>
2020-11-21 15:52:30 -0500

    Added first level basics, subject to updates

---
+++ Top Down RPG/Assets/Scripts/TransformLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x > 20.4)
+        {
+            this.transform.position = new Vector2(-12.5f, this.transform.position.y);
+        }
+        Vector2 xPosition = this.transform.position;
+        xPosition.x += 0.001f;
+        this.transform.position = xPosition;
+    }
+}
