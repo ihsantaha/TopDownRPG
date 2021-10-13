d866432ab5023563bc02a56cc58bb001f252c760
Ihsan <ihsantaha999@gmail.com>
2021-10-13 07:07:01 -0500

    Testing Stage View Builder

---
+++ TopDownRPG/Assets/Scripts/SceneTransitionUtil.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionUtil : MonoBehaviour
+{
+    public SpriteRenderer render;
+    public bool automaticTransition;
+    public bool fadeInOnly;
+    public bool fadeOutOnly;
+    public float startFadeInCountdown;
+    public float startFadeOutCountdown;
+    public float fadeInDuration;
+    public float fadeOutDuration;
+    public float rateOfChange;
+
+    void Start()
+    {
+        render = gameObject.GetComponent<SpriteRenderer>();
+        fadeInDuration = 0.95f;
+        fadeOutDuration = 1.05f;
+        rateOfChange = 0.0005f;
+
+        if (automaticTransition)
+        {
+            if (fadeInOnly)
+                StartCoroutine(FadeInOnly(fadeInDuration));
+            else if (fadeOutOnly)
+                StartCoroutine(FadeOutOnly(fadeOutDuration));
+            else
+                StartCoroutine(FadeInAndOut(fadeInDuration, fadeOutDuration));
+        }
+    }
+
+    public IEnumerator FadeInOnly(float fadeInDuration)
+    {
+        yield return new WaitForSeconds(startFadeInCountdown);
+
+        for (float alpha = fadeInDuration; alpha >= -rateOfChange; alpha -= rateOfChange)
+        {
+            UpdateFrame(alpha);
+            yield return new WaitForSeconds(rateOfChange);
+        }
+    }
+
+    public IEnumerator FadeOutOnly(float fadeOutDuration)
+    {
+        UpdateFrame(0);
+        yield return new WaitForSeconds(startFadeOutCountdown);
+
+        for (float alpha = rateOfChange; alpha <= fadeOutDuration; alpha += rateOfChange)
+        {
+            UpdateFrame(alpha);
+            yield return new WaitForSeconds(rateOfChange);
+        }
+    }
+
+    public IEnumerator FadeInAndOut(float fadeInDuradion, float fadeOutDuration)
+    {
+        yield return new WaitForSeconds(startFadeInCountdown);
+
+        for (float alpha = fadeInDuration; alpha >= -0.05; alpha -= rateOfChange)
+        {
+            UpdateFrame(alpha);
+            yield return new WaitForSeconds(rateOfChange);
+        }
+        yield return new WaitForSeconds(startFadeOutCountdown - (startFadeInCountdown + fadeInDuration));
+        for (float alpha = rateOfChange; alpha <= fadeOutDuration; alpha += rateOfChange)
+        {
+            UpdateFrame(alpha);
+            yield return new WaitForSeconds(rateOfChange);
+        }
+    }
+
+    public void UpdateFrame(float f)
+    {
+        Color c = render.material.color;
+        c.a = f;
+        render.material.color = c;
+    }
+
+}
---
+++ TopDownRPG/Assets/Scripts/SkyEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyEditor : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    int spriteIndex;
+    public Sprite[] skySprites;
+
+
+    void Start()
+    {
+        spriteIndex = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = skySprites[spriteIndex];
+        
+
+        // Coroutine
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //InvokeRepeating("changeTimeOfDay", 1f, 1f);
+    }
+
+    void changeTimeOfDay()
+    {  
+        // Upgrade to use fading
+
+        if (spriteIndex <= skySprites.Length)
+        {
+            spriteIndex++;
+        } else
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = skySprites[spriteIndex];
+    }
+}
